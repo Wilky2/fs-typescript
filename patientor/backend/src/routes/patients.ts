2c8b9316b25d4843704ca36b126afab420e94087
Wilky2 <wilkyclervil@yahoo.com>
2026-04-17 17:47:08 -0400

    exercise 15 complete

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,26 +1,36 @@
-import express, { type Response } from 'express';
+import express, { type Request, type NextFunction, type Response } from 'express';
 import patientService from '../services/patientService.ts';
-import { type NonSensitivePatient } from '../type.ts';
-import { parseNewPatientEntry } from '../utils.ts';
+import { NewEntrySchema, type NewPatient, type NonSensitivePatient } from '../type.ts';
+import { z } from 'zod';
 
 const router = express.Router();
+
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+    try {
+        NewEntrySchema.parse(req.body);
+        next();
+    } catch (error: unknown) {
+        next(error);
+    }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof z.ZodError) {
+        res.status(400).send({ error: error.issues });
+    } else {
+        next(error);
+    }
+};
 
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
     res.send(patientService.getNonSensitiveEntries());
 });
 
-router.post('/', (req, res) => {
-    try {
-        const newPatientEntry = parseNewPatientEntry(req.body);
-        const addedEntry = patientService.addPatient(newPatientEntry);
-        res.json(addedEntry);
-    } catch (error: unknown) {
-        let errorMessage = 'Something went wrong.';
-        if (error instanceof Error) {
-            errorMessage += ' Error: ' + error.message;
-        }
-        res.status(400).send(errorMessage);
-    }
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatient>, res: Response<NonSensitivePatient>) => {
+    const addedEntry = patientService.addPatient(req.body);
+    res.json(addedEntry);
 });
+
+router.use(errorMiddleware);
 
 export default router;
